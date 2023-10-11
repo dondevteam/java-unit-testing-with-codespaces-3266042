9b38a5f8c44c259c28ad160f0706cfe39bf7c429
Don Phelix <141995407+dondevteam@users.noreply.github.com>
2023-10-11 09:23:38 +0000

    lesson learned from assert arrays equals

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,6 +1,10 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CalculatorTest {
 
@@ -38,6 +42,40 @@ public class CalculatorTest {
         int expected = 2;
 
         int actual = calculator.divide(6, 3);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testIsEven() {
+        boolean actual = calculator.isEven(2);
+
+        assertTrue(actual);
+    }
+
+    @Test
+    public void testIsOdd() {
+        boolean actual = calculator.isEven(1);
+
+        assertFalse(actual);
+    }
+
+    @Test
+    public void testIncrementArray() {
+        int[] expected = new int[] { 2, 3, 4 };
+
+        int[] actual = calculator.incrementArray(new int[] { 1, 2, 3 });
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testDivideByZero() {
+        Exception exception =  assertThrows(IllegalArgumentException.class, () -> calculator.divide(1, 0));
+
+        String expected = "Ints cannot be divided by zero";
+
+        String actual = exception.getMessage();
 
         assertEquals(expected, actual);
     }
